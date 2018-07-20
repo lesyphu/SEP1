56ac47437aeeb1a6494d566092df382f775d6404
lesyphu <syphule1596@gmail.com>
2018-07-20 16:16:08 +0700

    empty website

---
+++ SEPApplication1/SEPApplication1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SEPApplication1.MvcApplication" Language="C#" %>
---
+++ SEPApplication1/SEPApplication1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEPApplication1.Startup))]
+namespace SEPApplication1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
