180079d784edebdaa53513adea3a74d64a1feb28
phamhaian <phamhaian20061997@gmail.com>
2018-07-23 14:52:30 +0700

    Create branch Members

---
+++ SEPApplication1/SEPApplication1/Controllers/MembersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEPApplication1.Controllers
+{
+    public class MembersController : Controller
+    {
+        //
+        // GET: /Members/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
