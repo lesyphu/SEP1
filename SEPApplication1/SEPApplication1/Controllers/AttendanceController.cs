8b7c1a4a4566b7e4be1cd3913e9c6ab894c9516f
thanhhuy3010 <huyhuythanh3010@gmail.com>
2018-07-25 14:05:08 +0700

    create Attendance branch

---
+++ SEPApplication1/SEPApplication1/Controllers/AttendanceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEPApplication1.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        //
+        // GET: /Attendance/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
