405cf4599c53983373c75b4f95c0f966a1d0af68
An Pham <haian20061997@gmail.com>
2018-08-01 16:37:10 +0700

    Member branch create and push full code

--- SEPApplication1/SEPApplication1/API.cs
+++ SEPApplication1/SEPApplication1/API.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Web;
+using SEPApplication1.Models;
 
 namespace SEPApplication1
 {
@@ -28,15 +29,63 @@ namespace SEPApplication1
     }
     public class GetCoursesResult
     {
-        public int  Code { get; set; }
+        public int Code { get; set; }
         public CoursesData[] Data { get; set; }
+        public string Message { get; set; }
+    }
+    public class GetMemberResult
+    {
+        public int Code { get; set; }
+        public MemberData[] Data { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class PostMemberResult
+    {
+        public int Code { get; set; }
+        public string message { get; set; }
+    }
+
+
+    public class MemberData
+    {
+        public string ID { get; set; }
+        public string fullname { get; set; }
+        public DateTime birthday { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+    }
+    public class StudentData
+    {
+        public string Id { get; set; }
+        public string Fullname { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+    public class GetStudentResult
+    {
+        public int Code { get; set; }
+        public StudentData Data { get; set; }
+        public string Message { get; set; }
+    }
+    public class SessionsData
+    {
+        public string ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Info { get; set; }
+    }
+    public class GetSessionResult
+    {
+        public int Code { get; set; }
+        public SessionsData[] Data { get; set; }
         public string Message { get; set; }
     }
 
     public class API
     {
-       
-        public LoginResult Login(string  username, string password)
+        //string URL = "http://125.234.238.137:8080/CMU/SEPAPI/SEP21/";
+        string URL = "http://cntttest.vanlanguni.edu.vn:8080/CMU/SEPAPI/SEP21/";
+        public LoginResult Login(string username, string password)
         {
             using (var client = new WebClient())
             {
@@ -48,13 +97,62 @@ namespace SEPApplication1
                 return JsonConvert.DeserializeObject<LoginResult>(json);
             }
         }
-        public GetCoursesResult GetCourses (string lecturerID)
+        public GetCoursesResult GetCourses(string lecturerID)
         {
-             using (var client = new WebClient())
-             {
-                var json = client.DownloadString("http://cntttest.vanlanguni.edu.vn:8080/CMU/SEPAPI/SEP21/"+ "/GetCourses?lecturerID="+ lecturerID);
+            using (var client = new WebClient())
+            {
+                var json = client.DownloadString("http://cntttest.vanlanguni.edu.vn:8080/CMU/SEPAPI/SEP21/" + "/GetCourses?lecturerID=" + lecturerID);
                 return JsonConvert.DeserializeObject<GetCoursesResult>(json);
-             }
+            }
+        }
+        public GetSessionResult GetSessions(string Course_id)
+        {
+            using (var client = new WebClient())
+            {
+                var json = client.DownloadString("http://cntttest.vanlanguni.edu.vn:8080/CMU/SEPAPI/SEP21/" + "/GetSessions?Course_id=" + Course_id);
+                return JsonConvert.DeserializeObject<GetSessionResult>(json);
+            }
+        }
+        //GetMember
+        public GetMemberResult GetMember(string courseID)
+        {
+            using (var client = new WebClient())
+            {
+                client.Encoding = System.Text.Encoding.UTF8;
+                var json = client.DownloadString(URL + "/GetMembers?courseID=" + courseID);
+                return JsonConvert.DeserializeObject<GetMemberResult>(json);
+            }
+        }
+        SEPEntities db = new SEPEntities();
+        //PostMember
+        public PostMemberResult postmember(string uid, string secret, string course)
+        {
+            using (var client = new WebClient())
+            {
+                var form = new NameValueCollection();
+                form["uid"] = uid;
+                form["secret"] = secret;
+
+                var datas = new
+                {
+                    course = course,
+                    members = db.Members.Where(x => x.Course.Code == course).Select(b => b.Code).ToArray()
+                };
+                var predata = JsonConvert.SerializeObject(datas);
+                form["data"] = predata;
+                var result = client.UploadValues(URL + "SyncMembers", "POST", form);
+                var json = Encoding.UTF8.GetString(result);
+                return JsonConvert.DeserializeObject<PostMemberResult>(json);
+            }
+        }
+        public GetStudentResult GetStudent(string code)
+        {
+            using (var client = new WebClient())
+            {
+                var json = client.DownloadString("http://cntttest.vanlanguni.edu.vn:8080/CMU/SEPAPI/SEP21/" + "GetStudent?code=" + code);
+                return JsonConvert.DeserializeObject<GetStudentResult>(json);
+
+            }
         }
     }
 }
---
+++ SEPApplication1/SEPApplication1/Controllers/MembersController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SEPApplication1.Models;
+
+namespace SEPApplication1.Controllers
+{
+    public class MembersController : Controller
+    {
+        //
+        // GET: /Members/
+        private SEPEntities db = new SEPEntities();
+        private API api = new API();
+        public ActionResult Index(int courseId)
+        {
+            ViewBag.CourseId = courseId;
+            var members = db.Members.Where(m => m.Course_id == courseId).ToList();
+            return View(members);
+        }
+        // GET: /Session/
+        //public ActionResult Index(int id)
+        //{
+        //    ViewBag.CourseId = id;
+        //    ViewBag.courseCode = db.Courses.FirstOrDefault(x => x.id == id).Code;
+        //    var member = db.Members.Where(s => s.Course_id == id);
+        //    return View(member.ToList());
+        //}
+
+        // GET: /Session/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member mb = db.Members.Find(id);
+            if (mb == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mb);
+        }
+
+        // GET: /Session/Create
+        public ActionResult Create(int courseId)
+        {
+            ViewBag.CourseId = courseId;
+            return View();
+        }
+
+        // POST: /Session/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(int courseId, Member member)
+        {
+            var result = new API().GetStudent(member.Code);
+            if (result.Code == 0)
+            {
+                member.Fullname = result.Data.Fullname;
+                member.Birthday = result.Data.Birthday;
+                member.Course_id = courseId;
+                db.Members.Add(member);
+                db.SaveChanges();
+                return RedirectToAction("Index", new { courseId = courseId });
+            }
+            //session.Course_id = courseId;
+            //string uid = Session["ID"].ToString();
+            //string secret = Session["Secret"].ToString();
+            //string course = db.Courses.FirstOrDefault(x => x.id == courseId).id.ToString();
+
+            //if (ModelState.IsValid)
+            //{
+            //    db.Members.Add(session);
+            //    db.SaveChanges();
+            //    var a = api.postmember(uid, secret, db.Courses.FirstOrDefault(x => x.id == courseId).Code);
+            //    return RedirectToAction("Index", new { courseId = courseId });
+            //}
+            ViewBag.CourseId = courseId;
+
+            return View(member);
+            //session.Course_id = courseId;
+            //string uid = Session["ID"].ToString();
+            //string secret = Session["Secret"].ToString();
+            //string course = db.Courses.FirstOrDefault(x => x.id == courseId).id.ToString();
+
+            //if (ModelState.IsValid)
+            //{
+            //    db.Members.Add(session);
+            //    db.SaveChanges();
+            //    var a = api.postmember(uid, secret, db.Courses.FirstOrDefault(x => x.id == courseId).Code);
+            //    return RedirectToAction("Index", new { id = courseId });
+            //}
+            //ViewBag.Course_id = courseId;
+            //return View(session);
+        }
+
+        public ActionResult SyncMember(string id)
+        {
+            var course = db.Courses.FirstOrDefault(x => x.Code == id);
+            var member = api.GetMember(id);
+            Member Nmember = new Member();
+            foreach (var item in member.Data)
+            {
+                Nmember.Course_id = course.id;
+                Nmember.Birthday = item.birthday;
+                Nmember.Fullname = item.fullname;
+                Nmember.Code = item.ID;
+                db.Members.Add(Nmember);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", new { id = course.id });
+        }
+
+        // GET: /Session/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Member session = db.Members.Find(id);
+        //    if (session == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.Course_id = new SelectList(db.Courses, "id", "Code", session.Course_id);
+        //    return View(session);
+        //}
+
+        //// POST: /Session/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "id,Date,Info,Course_id")] Session session)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(session).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.Course_id = new SelectList(db.Courses, "id", "Code", session.Course_id);
+        //    return View(session);
+        //}
+
+        //// GET: /Session/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Session session = db.Sessions.Find(id);
+        //    if (session == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(session);
+        //}
+
+        //// POST: /Session/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Session session = db.Sessions.Find(id);
+        //    db.Sessions.Remove(session);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
